0cda1511a9689276cbf95362357c93bb74221dc1
Aleksandr <65610825+A-Apkhanov@users.noreply.github.com>
2022-05-23 08:11:52 +0300

    add views and route (#1)

---
+++ src/views/CategoriesView.vue
@@ -0,0 +1,94 @@
+<template>
+  <div>
+    <div class="page-title">
+      <h3>Категории</h3>
+    </div>
+    <section>
+      <div class="row">
+        <div class="col s12 m6">
+          <div>
+            <div class="page-subtitle">
+              <h4>Создать</h4>
+            </div>
+
+            <form>
+              <div class="input-field">
+                <input
+                  id="create-name"
+                  type="text"
+                >
+                <!--                eslint-disable-next-line-->
+                <label for="create-name">Название</label>
+                <span class="helper-text invalid">Введите название</span>
+              </div>
+
+              <div class="input-field">
+                <input
+                  id="create-limit"
+                  type="number"
+                >
+                <!--                eslint-disable-next-line-->
+                <label for="create-limit">Лимит</label>
+                <span class="helper-text invalid">Минимальная величина</span>
+              </div>
+
+              <button
+                class="btn waves-effect waves-light"
+                type="submit"
+              >
+                Создать
+                <i class="material-icons right">send</i>
+              </button>
+            </form>
+          </div>
+        </div>
+        <div class="col s12 m6">
+          <div>
+            <div class="page-subtitle">
+              <h4>Редактировать</h4>
+            </div>
+
+            <form>
+              <div class="input-field">
+                <select id="category">
+                  <option>Category</option>
+                </select>
+                <!--                eslint-disable-next-line-->
+                <label for="category">Выберите категорию</label>
+              </div>
+
+              <div class="input-field">
+                <input
+                  type="text"
+                  id="edit-name"
+                >
+                <!--                eslint-disable-next-line-->
+                <label for="edit-name">Название</label>
+                <span class="helper-text invalid">TITLE</span>
+              </div>
+
+              <div class="input-field">
+                <label for="edit-limit">
+                  <input
+                    id="edit-limit"
+                    type="number"
+                  >
+                  Лимит
+                </label>
+                <span class="helper-text invalid">LIMIT</span>
+              </div>
+
+              <button
+                class="btn waves-effect waves-light"
+                type="submit"
+              >
+                Обновить
+                <i class="material-icons right">send</i>
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
