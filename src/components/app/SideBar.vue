d226fc7e29977e38f501574d58090283e27ff39f
A-Apkhanov <apkhanov.a.s@gmail.com>
2022-05-24 22:26:50 +0300

    add router-link to sidebar and toggle show sidebar

---
+++ src/components/app/SideBar.vue
@@ -0,0 +1,61 @@
+<template>
+  <ul
+    class="sidenav app-sidenav"
+    :class="{open: isOpenSidebar}"
+  >
+    <router-link
+      v-for="link in links"
+      :key="link.url"
+      :to="link.url"
+      custom
+      v-slot="{ href, navigate, isActive}"
+    >
+      <li
+        :class="{active: isActive}"
+      >
+        <a
+          :href="href"
+          @click="navigate"
+          class="waves-effect waves-orange pointer"
+        >
+          {{ link.title }}
+        </a>
+      </li>
+    </router-link>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'SideBar',
+  props: {
+    isOpenSidebar: Boolean,
+  },
+  data() {
+    return {
+      links: [
+        {
+          title: 'Счет',
+          url: '/',
+        },
+        {
+          title: 'История',
+          url: '/history',
+        },
+        {
+          title: 'Планирование',
+          url: '/planning',
+        },
+        {
+          title: 'Новая запись',
+          url: '/record',
+        },
+        {
+          title: 'Категории',
+          url: '/categories',
+        },
+      ],
+    };
+  },
+};
+</script>
