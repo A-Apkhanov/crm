649df1a38b886206e50aeaf26888283c14d6312a
A-Apkhanov <apkhanov.a.s@gmail.com>
2022-07-04 21:02:31 +0300

    page for display planning

--- src/store/record.js
+++ src/store/record.js
@@ -1,6 +1,19 @@
-import { getDatabase, ref, push } from 'firebase/database';
+import {
+  getDatabase, ref, push, get, child,
+} from 'firebase/database';
 
 export default {
+  state: {
+    records: [],
+  },
+  mutations: {
+    setRecords(state, records) {
+      state.records = records;
+    },
+    clearRecords(state) {
+      state.records = [];
+    },
+  },
   actions: {
     async createRecord({ dispatch, commit }, data) {
       try {
@@ -12,5 +25,26 @@ export default {
         throw e;
       }
     },
+    async getRecords({ dispatch, commit }) {
+      try {
+        const uid = await dispatch('getUid');
+        const dbRef = ref(getDatabase());
+        const records = await get(child(dbRef, `users/${uid}/records`))
+          .then((snapshot) => snapshot.val());
+        if (records) {
+          commit(
+            'setRecords',
+            Object.keys(records)
+              .map((key) => ({ ...records[key], id: key })),
+          );
+        }
+      } catch (e) {
+        commit('setError', e);
+        throw e;
+      }
+    },
+  },
+  getters: {
+    records: (state) => state.records,
   },
 };
--- src/views/PlanningView.vue
+++ src/views/PlanningView.vue
@@ -2,22 +2,107 @@
   <div>
     <div class="page-title">
       <h3>Планирование</h3>
-      <h4>12 212</h4>
+      <h4>{{ bill }}</h4>
     </div>
 
-    <section>
-      <div>
+    <LoaderIcon v-if="isLoading" />
+
+    <p
+      class="center"
+      v-else-if="!records.length"
+    >
+      Категорий пока нет.
+      <router-link to="/categories">
+        Добавить новую категорию.
+      </router-link>
+    </p>
+
+    <section v-else>
+      <div
+        v-for="category of categories"
+        :key="category.id"
+      >
         <p>
-          <strong>Девушка:</strong>
-          12 122 из 14 0000
+          <strong>{{ category.title }}:</strong>
+          {{ displayCurrency(category.spend) }} из {{ displayCurrency(category.limit) }}
         </p>
-        <div class="progress">
+        <div
+          class="progress"
+          v-tooltip="category.tooltip"
+        >
           <div
-            class="determinate green"
-            style="width:40%"
+            class="determinate"
+            :class="[category.progressColor]"
+            :style="{ width:category.progressPercent + '%' }"
           />
         </div>
       </div>
     </section>
   </div>
 </template>
+
+<script>
+import LoaderIcon from '@/components/app/LoaderIcon.vue';
+import formatCurrency from '@/services/formatCurrency';
+
+export default {
+  name: 'PlanningView',
+  components: { LoaderIcon },
+  data() {
+    return {
+      isLoading: true,
+    };
+  },
+  computed: {
+    categories() {
+      const { categories } = this.$store.getters;
+      return categories.map((category) => {
+        const spend = this.records
+          .filter((record) => record.categoryId === category.id)
+          .filter((record) => record.type === 'outcome')
+          .reduce((total, record) => total + +record.amount, 0);
+        const percent = (100 * spend) / category.limit;
+        const progressPercent = percent > 100 ? 100 : percent;
+        let progressColor;
+        if (percent < 60) {
+          progressColor = 'green';
+        } else if (percent < 100) {
+          progressColor = 'yellow';
+        } else {
+          progressColor = 'red';
+        }
+
+        const tooltipValue = category.limit - spend;
+        const tooltip = `${tooltipValue < 0 ? 'Превышение на' : 'Осталось'} ${
+          formatCurrency(Math.abs(tooltipValue))}`;
+
+        return {
+          ...category,
+          progressColor,
+          progressPercent,
+          spend,
+          tooltip,
+        };
+      });
+    },
+    records() {
+      return this.$store.getters.records;
+    },
+    bill() {
+      const { info } = this.$store.getters;
+      const bill = info ? info.bill : 0;
+      return formatCurrency(bill);
+    },
+  },
+  methods: {
+    displayCurrency(currency) {
+      return formatCurrency(currency);
+    },
+  },
+  async mounted() {
+    await this.$store.dispatch('getCategories');
+    await this.$store.dispatch('getRecords');
+    this.isLoading = false;
+  },
+};
+</script>
