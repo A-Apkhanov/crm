91607f6b0215b6a4dbcaed2ddcc18a66c6bcfd89
A-Apkhanov <apkhanov.a.s@gmail.com>
2022-07-07 18:44:43 +0300

    add history table

--- src/views/HistoryView.vue
+++ src/views/HistoryView.vue
@@ -8,36 +8,57 @@
       <canvas />
     </div>
 
-    <section>
-      <table>
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>Сумма</th>
-            <th>Дата</th>
-            <th>Категория</th>
-            <th>Тип</th>
-            <th>Открыть</th>
-          </tr>
-        </thead>
+    <LoaderIcon v-if="isLoading" />
 
-        <tbody>
-          <tr>
-            <td>1</td>
-            <td>1212</td>
-            <td>12.12.32</td>
-            <td>name</td>
-            <td>
-              <span class="white-text badge red">Расход</span>
-            </td>
-            <td>
-              <button class="btn-small btn">
-                <i class="material-icons">open_in_new</i>
-              </button>
-            </td>
-          </tr>
-        </tbody>
-      </table>
+    <p
+      class="center"
+      v-else-if="!records.length"
+    >
+      Записей пока нет.
+      <router-link to="/record">
+        Добавьте первую запись.
+      </router-link>
+    </p>
+
+    <section v-else>
+      <HistoryTable :records="records" />
     </section>
   </div>
 </template>
+
+<script>
+import HistoryTable from '@/components/app/HistoryTable.vue';
+import LoaderIcon from '@/components/app/LoaderIcon.vue';
+
+export default {
+  name: 'HistoryView',
+  components: { LoaderIcon, HistoryTable },
+  data() {
+    return {
+      isLoading: true,
+    };
+  },
+  computed: {
+    categories() {
+      return this.$store.getters.categories;
+    },
+    records() {
+      const { records } = this.$store.getters;
+      return records
+        .map((record) => ({
+          ...record,
+          categoryName: this.categories
+            .find((category) => category.id === record.categoryId).title,
+          typeClass: record.type === 'income' ? 'green' : 'red',
+          typeText: record.type === 'income' ? 'Доход' : 'Расход',
+        }))
+        .reverse();
+    },
+  },
+  async mounted() {
+    await this.$store.dispatch('getCategories');
+    await this.$store.dispatch('getRecords');
+    this.isLoading = false;
+  },
+};
+</script>
