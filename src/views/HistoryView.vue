935581bba6c84953b653cbab58c6e80e52744dd2
A-Apkhanov <apkhanov.a.s@gmail.com>
2022-07-10 11:57:21 +0300

    add pagination to history page

--- src/views/HistoryView.vue
+++ src/views/HistoryView.vue
@@ -12,7 +12,7 @@
 
     <p
       class="center"
-      v-else-if="!records.length"
+      v-else-if="!lengthRecords"
     >
       Записей пока нет.
       <router-link to="/record">
@@ -21,43 +21,51 @@
     </p>
 
     <section v-else>
-      <HistoryTable :records="records" />
+      <HistoryTable :records="items" />
+
+      <v-pagination
+        v-model="page"
+        :pages="pageCount"
+        :range-size="1"
+        active-color="#e57373"
+        @update:modelValue="handleChangePage"
+      />
     </section>
   </div>
 </template>
 
 <script>
+import VPagination from '@hennge/vue3-pagination';
+import '@hennge/vue3-pagination/dist/vue3-pagination.css';
+
 import HistoryTable from '@/components/HistoryTable.vue';
 import LoaderIcon from '@/components/app/LoaderIcon.vue';
+import paginationMixin from '@/mixins/pagination.mixin';
 
 export default {
   name: 'HistoryView',
-  components: { LoaderIcon, HistoryTable },
+  components: { LoaderIcon, HistoryTable, VPagination },
+  mixins: [paginationMixin],
   data() {
     return {
       isLoading: true,
+      lengthRecords: 0,
     };
-  },
-  computed: {
-    categories() {
-      return this.$store.getters.categories;
-    },
-    records() {
-      const { records } = this.$store.getters;
-      return records
-        .map((record) => ({
-          ...record,
-          categoryName: this.categories
-            .find((category) => category.id === record.categoryId).title,
-          typeClass: record.type === 'income' ? 'green' : 'red',
-          typeText: record.type === 'income' ? 'Доход' : 'Расход',
-        }))
-        .reverse();
-    },
   },
   async mounted() {
     await this.$store.dispatch('getCategories');
     await this.$store.dispatch('getRecords');
+    const { records, categories } = this.$store.getters;
+    this.lengthRecords = records.length;
+    this.setupPagination(records.map((record) => ({
+      ...record,
+      categoryName: categories
+        .find((category) => category.id === record.categoryId).title,
+      typeClass: record.type === 'income' ? 'green' : 'red',
+      typeText: record.type === 'income' ? 'Доход' : 'Расход',
+    }))
+      .reverse());
+
     this.isLoading = false;
   },
 };
